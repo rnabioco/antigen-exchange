674229370705253d3e4304341949c4ae228496b1
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2024-06-20 12:48:07 -0600

    updated xenium analysis

---
+++ results/xenium/knit_rmd.R
@@ -0,0 +1,23 @@
+library(rmarkdown)
+library(tidyverse)
+library(here)
+
+slides <- c(
+  "1" = "output-XETG00230__0022624__[A-Z]__",
+  "2" = "output-XETG00230__0022841__[A-Z]__"
+)
+
+slides %>%
+  iwalk(~ {
+    output <- str_c("xenium-slide-", .y, ".html")
+
+    render(
+      input       = "xenium-analysis.Rmd",
+      output_file = output,
+      params      = list(
+        slide_id    = .y,
+        slide_regex = .x 
+      )
+    )
+  })
+
