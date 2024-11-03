4b0f68ca803d34b74593f547191d88fdff628a7d
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2024-11-03 15:15:08 -0700

    geomx geo data

--- results/geo/geo_geomx_submission.sh
+++ results/geo/geo_geomx_submission.sh
@@ -12,41 +12,58 @@ mkdir -p "$sub"
 
 tmp=$(mktemp tmp.XXXXX)
 
-# pkc files
-for file in "$ref_dir/"*.pkc 
+# RUN THIS ON CLUSTER
+# # pkc files
+# for file in "$ref_dir/"*.pkc 
+# do
+#     ln -sr "$file" "$sub"
+# 
+#     md5sum "$file" \
+#         >> "$tmp"
+# done
+# 
+# # GeoMx worksheet and object metadata
+# for file in "../geomx/geomx_worksheets/Tamburini_061423_20230619T1930_LabWorksheet.txt" "geomx_metadata.tsv.gz"
+# do
+#     ln -sr "$file" "$sub"
+#     
+#     md5sum "$file" \
+#         >> "$tmp"
+# done
+# 
+# # fastqs
+# for dat in ${data[@]}
+# do
+#     fq=$dat_dir/$dat/*.fastq.gz
+# 
+#     ln -sr $fq "$sub"
+# 
+#     cat "$dat_dir/$dat/md5sums.txt" \
+#         | sort -k2,2 \
+#         | awk '$2 !~ ".csv$"' \
+#         >> $tmp
+# done
+# 
+# # format md5sums
+# cat $tmp \
+#     | awk -v OFS="  " '{gsub("^.*/", "", $2); print}' \
+#     > $sums
+
+# RUN THIS LOCALLY
+# dcc files
+# * run this locally since dcc files are not on cluster
+for file in ../geomx/counts/*.dcc
 do
     ln -sr "$file" "$sub"
 
     md5sum "$file" \
         >> "$tmp"
-done
-
-# GeoMx worksheet and object metadata
-for file in "../geomx/geomx_worksheets/Tamburini_061423_20230619T1930_LabWorksheet.txt" "geomx_metadata.tsv.gz"
-do
-    ln -sr "$file" "$sub"
-    
-    md5sum "$file" \
-        >> "$tmp"
-done
-
-# fastqs
-for dat in ${data[@]}
-do
-    fq=$dat_dir/$dat/*.fastq.gz
-
-    ln -sr $fq "$sub"
-
-    cat "$dat_dir/$dat/md5sums.txt" \
-        | sort -k2,2 \
-        | awk '$2 !~ ".csv$"' \
-        >> $tmp
 done
 
 # format md5sums
-cat $tmp \
+cat "$tmp" \
     | awk -v OFS="  " '{gsub("^.*/", "", $2); print}' \
-    > $sums
+    >> "$sums"
 
-rm $tmp
+rm "$tmp"
 
