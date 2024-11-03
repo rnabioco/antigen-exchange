b14996c7d4498c1049f7000a2dae77931831b952
Ryan Sheridan <ryan.sheridan@cuanschutz.edu>
2024-11-02 19:16:01 -0600

    xenium geo files wip

--- results/geo/geo_xenium_submission.sh
+++ results/geo/geo_xenium_submission.sh
@@ -1,85 +1,33 @@
 #! /bin/usr/env bash
 
-data=('211230_novogene' '220128_A00405_0521_AHH7JVDSX3' '220311_A00405_0541_AHCMMHDSX3' '221028_A00405_0632_AHJTM7DSX5')
-dat_dir='../../data'
-ref_dir='../../ref'
-res_dir=("$HOME/Projects/tamburini-antigen-tracking/results/2022-03-11" "$HOME/Projects/tamburini-antigen-tracking/results/2022-10-28")
-sub='20241101_sheridan_scrnaseq'
-sums='metadata/geo_scrnaseq_md5sums.txt'
+dat_dir='../xenium/20240322__194506__032224_Tamburini_Run_1'
+sub='20241101_sheridan_xenium'
+sums='metadata/geo_xenium_md5sums.txt'
 
 set -o errexit -o pipefail -o nounset -x
 
 mkdir -p "$sub"
 
-# cellranger matrices
+# raw data
 tmp=$(mktemp tmp.XXXXX)
 
-nms=(BT_GEX_3-BT_ADT_3
-    BT_GEX_4-BT_ADT_4
-    BT_GEX_5-BT_ADT_5
-    BT_GEX_1-BT_ADT_1
-    BT_GEX_2-BT_ADT_2
-    BT_GEX_8-BT_ADT_8
-    BT_GEX_7-BT_ADT_7
-    BT_GEX_6-BT_ADT_6
-    stromal_P2-JH300_1
-    stromal_P4-JH300_2
-    stromal_P4_P2-JH300_3
-    DC_P2-JH300_4
-    DC_P4-JH300_5
-    DC_P4_P2-JH300_6)
+dirs="$dat_dir/"*__?__*
 
-for res in ${res_dir[@]}
+for dir in ${dirs[@]}
 do
-    for nm in ${nms[@]}
+    dir=$(basename "$dir")
+
+    for file in 'transcripts.csv.gz' "$dat_dir/$dir/"*.ome.tif
     do
-        run=$(basename $res)
-        dir="$res/$nm"
-        mat_dir="$res/$nm/outs"
-    
-        if [ ! -d "$dir" ]
-        then
-            continue
-        fi
+        file=$(basename "$file")
+        nm=$(echo "$dir" | cut -d '_' -f 3-5)
+        nm="${nm}_${file}"
 
-        for file in 'filtered_feature_bc_matrix.h5'
-        do
-            mat="$sub/${nm}_$file"
-    
-            ln -sr "$mat_dir/$file" "$mat"
-    
-            md5sum "$mat" \
-                >> "$tmp"
-        done
+        ln -sr "$dat_dir/$dir/$file" "$sub/$nm"
+
+        md5sum "$sub/$nm" \
+            >> "$tmp"
     done
-done
-
-# Seurat metadata
-for file in *_count_matrix.h5 *_metadata.tsv.gz
-do
-    ln -sr "$file" "$sub"
-
-    md5sum "$file" \
-        >> "$tmp"
-done
-
-# Feature reference
-ln -sr "$ref_dir/antibodies.csv" "$sub/feature_reference.csv"
-
-md5sum "$sub/feature_reference.csv" \
-    >> "$tmp"
-
-# fastqs
-for dat in ${data[@]}
-do
-    fq=$dat_dir/$dat/*.*q.gz
-
-    ln -sr $fq "$sub"
-
-    cat "$dat_dir/$dat/md5sums.txt" \
-        | sort -k2,2 \
-        | awk '$2 !~ ".csv$"' \
-        >> $tmp
 done
 
 # format md5sums
